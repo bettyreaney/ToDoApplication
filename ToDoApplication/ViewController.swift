ecdcbf3b0564b879f3ad4b0322a1110d052799b7
bettyreaney <breaney@mail.sfsu.edu>
2018-04-01 10:17:34 -0700

    Add button takes us to new View where you can add a task

--- ToDoApplication/ViewController.swift
+++ ToDoApplication/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     var tasks: [Task] = []
 
     override func viewDidLoad() {
+        
+        tasks.append(Task(name: "Test object"))
 
     }
     
